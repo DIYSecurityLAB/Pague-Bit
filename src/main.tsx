db0eeb0e8f92d67e6b632175ff0dcbf6e22bc318
Jefferson Laurindo da Silva <jefferson.laurindo@luzagroup.com>
2025-02-25 12:17:55 -0300

    refactor: use absolute paths on import

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
+import '@/domain/i18n/index.ts';
+import App from '@/view/routes/App.tsx';
+import '@/view/style/index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './domain/i18n/index.ts';
-import App from './view/routes/App.tsx';
-import './view/style/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
