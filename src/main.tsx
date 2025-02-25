ae205d0334840e8307e663f5b5d5f180d678532c
Jefferson Laurindo da Silva <jefferson.laurindo@luzagroup.com>
2025-02-25 12:29:04 -0300

    feat: add browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import '@/domain/i18n/index.ts';
-import App from '@/view/routes/App.tsx';
+import { App } from '@/view/App';
 import '@/view/style/index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 );
