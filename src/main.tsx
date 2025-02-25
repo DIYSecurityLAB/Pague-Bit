31265b92269d95d61854c0c40f250dd09f50c772
Jefferson Laurindo da Silva <jefferson.laurindo@luzagroup.com>
2025-02-25 12:44:43 -0300

    refactor: rename style to styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import '@/domain/i18n/index.ts';
 import { App } from '@/view/App';
-import '@/view/style/index.css';
+import '@/view/styles/index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
