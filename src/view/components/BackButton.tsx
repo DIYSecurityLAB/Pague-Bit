4f573e895d5800091b2620c877df01fe444c8092
Lucasqc04 <lucasqcampos9@gmail.com>
2025-02-26 07:22:06 -0300

    feat(site): add whatsapp button

--- src/view/components/BackButton.tsx
+++ src/view/components/BackButton.tsx
@@ -11,7 +11,7 @@ const BackButton = () => {
   return (
     <button
       onClick={handleBack}
-      className="fixed top-20 left-6 z-50 flex items-center gap-2 bg-transparent border border-orange-200 rounded-full shadow-md p-3 hover:bg-orange-50 transition"
+      className="fixed top-20 left-6 z-50 flex items-center gap-2 bg-transparent border-none rounded-full p-3 hover:bg-orange-50 transition"
     >
       <FiArrowLeft size={20} className="text-orange-500" />
       <span className="text-orange-500 font-medium">
--- src/view/pages/layout/Layout.tsx
+++ src/view/pages/layout/Layout.tsx
@@ -1,6 +1,7 @@
+import WhatsAppButton from '@/view/components/buttonWhatsApp';
 import React from 'react';
-import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -10,11 +11,12 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen bg-white text-gray-800">
       <Navbar />
-      <main className="flex-grow pt-[60px]">{children}</main> {/* Ajuste fino no espaçamento */}
+      <main className="flex-grow pt-[60px]">{children}</main>{' '}
+      {/* Ajuste fino no espaçamento */}
+      <WhatsAppButton />
       <Footer />
     </div>
   );
 };
 
 export default Layout;
-
